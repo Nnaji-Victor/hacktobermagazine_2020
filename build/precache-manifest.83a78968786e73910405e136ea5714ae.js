ec8aeb0301815f55b9460e5ceb31e4958f79a88d
Victor Nnaji <nnajivictor0@gmail.com>
2020-10-01 22:26:19 +0100

    fix build error

---
+++ build/precache-manifest.83a78968786e73910405e136ea5714ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacbdf71ca80d95ed635eb85e8d83b62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cedec10275887a09384",
+    "url": "/static/css/main.1dc531ec.chunk.css"
+  },
+  {
+    "revision": "aedfc83654bf3d00d90a",
+    "url": "/static/js/2.9e63fe0b.chunk.js"
+  },
+  {
+    "revision": "c5da8772d6113e069d11",
+    "url": "/static/js/3.a83e646f.chunk.js"
+  },
+  {
+    "revision": "e10cd95616206c96e655",
+    "url": "/static/js/4.46908fc5.chunk.js"
+  },
+  {
+    "revision": "0cedec10275887a09384",
+    "url": "/static/js/main.1582d2bc.chunk.js"
+  },
+  {
+    "revision": "f1c25da9938f999f7e1b",
+    "url": "/static/js/runtime~main.d006fb7f.js"
+  }
+]);
